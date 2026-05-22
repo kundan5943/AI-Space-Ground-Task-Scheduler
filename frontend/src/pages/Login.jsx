08e65a633afb159182a3ec06a2e803342be0afa6
Kundan Chaudhari <kundanchau624@gmail.com>
2026-05-22 19:43:05 +0530

    removed errors

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -37,6 +37,8 @@ const Login = () => {
       const data = await response.json();
 
       if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+
         navigate("/");
       } else {
         alert(data.message);
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,6 +5,7 @@ const SignUp = () => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
+    name: "",
     email: "",
     password: "",
   });
@@ -38,6 +39,8 @@ const SignUp = () => {
       const data = await response.json();
 
       if (response.ok) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+
         navigate("/");
       } else {
         alert(data.message);

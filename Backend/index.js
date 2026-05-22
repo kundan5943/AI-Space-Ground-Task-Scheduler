eed94545343421007e75683f5c2ff0b8bb6147cc
Kundan Chaudhari <kundanchau624@gmail.com>
2026-05-22 17:29:10 +0530

    added live server url in cors

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-space-ground-task-scheduler-frontend.onrender.com/",
+    ],
     credentials: true,
   }),
 );
